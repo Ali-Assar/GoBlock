3fa53cd661804db8683ea77643e69c2d108066f3
ali <ali.assar@skiff.com>
2024-02-12 10:32:46 +0330

    a test for Hashblock func along with a util file for creating a random block is added

--- types/block_test.go
+++ types/block_test.go
@@ -1,1 +1,15 @@
 package types
+
+import (
+	"testing"
+
+	"github.com/Ali-Assar/GoBlock/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHashBlok(t *testing.T) {
+	block := util.RandomBlock()
+	hash := HashBlock(block)
+
+	assert.Equal(t, 32, len(hash))
+}
