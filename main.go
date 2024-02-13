f2b9f931a80a1766453463ea89c825f619b721bd
ali <ali.assar@skiff.com>
2024-02-13 10:15:42 +0330

    grpc server is added and transaction can be made

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/Ali-Assar/GoBlock/node"
+	"github.com/Ali-Assar/GoBlock/proto"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("hello")
+	node := node.NewNode()
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+	ln, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proto.RegisterNodeServer(grpcServer, node)
+	fmt.Println("node running on port: ", "3000")
+
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+			makeTransaction()
+		}
+	}()
+	grpcServer.Serve(ln)
+}
+
+func makeTransaction() {
+	fmt.Println("-------")
+	client, err := grpc.Dial(":3000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := proto.NewNodeClient(client)
+
+	tx := &proto.Transaction{
+		Version: 1,
+	}
+	_, err = c.HandleTransaction(context.TODO(), tx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- node/node.go
+++ node/node.go
@@ -1,15 +1,23 @@
-package server
+package node
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Ali-Assar/GoBlock/proto"
+	"google.golang.org/grpc/peer"
 )
 
 type Node struct {
 	proto.UnimplementedNodeServer
 }
 
+func NewNode() *Node {
+	return &Node{}
+}
+
 func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*proto.None, error) {
-	return nil, nil
+	peer, _ := peer.FromContext(ctx)
+	fmt.Println("received tx from:", peer)
+	return &proto.None{}, nil
 }
